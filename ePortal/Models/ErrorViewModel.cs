d6188d256c3d3ae6d6083e7ec11777a2f9d6d176
tanveer2287 <tanveer_shaikh@jltasia.com>
2018-10-05 18:56:21 +0530

    Add project files.

---
+++ ePortal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ePortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
